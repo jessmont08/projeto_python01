50d46113c2f4aad3a9b89d76220a2defb43fcd68
jessmont08 <ster40028922@gmail.com>
2024-10-24 13:28:12 -0300

    saida formatada

--- terceira_aula03/entrada_de_dados02.py
+++ terceira_aula03/entrada_de_dados02.py
@@ -1,37 +1,45 @@
+# importa bibliotecas
 import os
 
 import datetime
 
+# puxar comando da biblioteca
 os.system('cls')
 
+ # titulo do sistema
 print('-' * 80)
 print('CADASTRO DE ENDEREÇO')
 print('=' * 80)
 
+# pedido de entrada do usuario
 nome = input('entre com seu nome: ')
 idade = input('entre com sua idade: ')
 cpf = input('entre com seu CPF: ')
 
+# pedido de f string 
 nascimento = int(input ('entre com seu ano de nascimento: '))
 estado = str(input('entre com seu estado:'))
 cidade = str(input('entre com sua cidade: '))
 bairro = str(input('entre com seu bairro: '))
 rua = str (input('entre com sua rua: '))
-numero = int (input('entre com o numero da sua casa'))
+numero = int (input('entre com o numero da sua casa: '))
 cep = int(input('entre com seu cep: '))
 
-
+# puxar comando da biblioteca
 ano_atual = datetime.datetime.now ( ).year
 idade = int(ano_atual) - nascimento
 
+# titulo da saida de dados
 print('-' * 80)
 print('SAIDA DE DADOS')
 print('=' * 80)
 
+# saida de dados
 print('N0ME: ' , nome)
 print('IDADE: ' , idade)
 print('CPF: ' , cpf) 
 
+# saida das  f string
 print(f'{nome}, você tem {idade} anos <3')
 print(f'CEP: {cep}')
 print(f'ESTADO: {estado}')
