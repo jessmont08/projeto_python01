2b62a5b0108dc96bede6848ca64f216fdfe4cb1b
jessmont08 <ster40028922@gmail.com>
2024-11-01 16:31:05 -0300

    bibliotecas

---
+++ sextaaula.py/matematica.py
@@ -0,0 +1,18 @@
+import os
+
+import math
+
+os.system('cls')
+
+print('-' * 90)
+print('ESTUDO DE BIBLIOTECA MATH')
+print('='* 90)
+
+base = 2
+expoente = 3
+angulo = 30
+radicando = 81
+ca = 5 #cateto adjacente
+co = 10 #cateto oposto
+
+potencia = math.pow
