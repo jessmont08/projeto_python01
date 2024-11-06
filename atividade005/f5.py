577abc7f8492f09932115c7fe911387950cfed23
jessmont08 <ster40028922@gmail.com>
2024-11-06 16:12:39 -0300

    exerciciodefuncaopt2

--- atividade005/f5.py
+++ atividade005/f5.py
@@ -1,1 +1,21 @@
-# Faça um programa que receba o nome completo de uma pessoa e, posteriormente, imprima esse nome separadamente.
+# Faça um programa que receba o nome completo de 
+# uma pessoa e, posteriormente, imprima esse nome 
+# separadamente.
+
+import os
+
+os.system('cls')
+
+print('-' * 90)
+print('NOME SEPARADAMENTE.')
+print('=' * 90)
+
+nome = str(input('NOME COMPLETO: '))
+
+nome_separado = nome.split()
+
+print(f'Bem vindo ao sistema, {nome_separado}.') 
+
+print('=' * 90)
+
+print()
--- atividade005/i5.py
+++ atividade005/i5.py
@@ -1,1 +1,22 @@
-# Faça um programa que receba o nome completo de uma pessoa e, em seguida, mostre o primeiro e o último nome.
+# Faça um programa que receba o nome completo 
+# de uma pessoa e, em seguida, mostre
+#  o primeiro e o último nome.
+
+import os
+
+os.system('cls')
+
+print('-' * 90)
+print('PRIMEIRO E ÙLTIMO NOME.')
+print('=' * 90)
+
+nome = str(input('Nome: '))
+
+lista = nome.split()
+
+nome1 = lista[:1]
+nome3 = lista[-1:]
+
+print(f'Nome: {nome1}, sobrenome: {nome3}.')
+print('-' * 90)
+print()
