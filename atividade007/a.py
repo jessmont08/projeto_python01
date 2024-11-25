eb297a7c2483079c68f03017a0f90721a96149cc
jessmont08 <ster40028922@gmail.com>
2024-11-25 16:33:15 -0300

    avulso

--- atividade007/a.py
+++ atividade007/a.py
@@ -1,7 +1,21 @@
-# Faça um programa que leia um número indeterminado de notas (pressione ‘s’ ou ‘0’ para sair). 
-# Após esta entrada de dados, faça o seguinte:
+
 # Mostre a quantidade de notas que foram lidas.
 # Exiba todas as notas na ordem em que foram informadas.
 # Exiba todas as notas na ordem inversa à que foram informadas, uma abaixo da outra.
 # Calcule e mostre a soma das notas.
 # Calcule e mostre a média das notas.
+
+while (True):
+
+    lista = [ ]
+
+    print('Digite "s" para sair.')
+    notas = str(input('Entre com as notas: '))
+
+    if notas != "s":
+        print(f'Digite a proxíma.')
+    else:
+        break
+    
+    lista.append(notas)
+
