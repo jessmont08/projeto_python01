cd34a1fa94645d4a2bbe01ead3e13a5fb5c25817
jessmont08 <ster40028922@gmail.com>
2024-11-26 14:28:21 -0300

    avulso

--- atividade007/a.py
+++ atividade007/a.py
@@ -5,12 +5,15 @@
 # Calcule e mostre a soma das notas.
 # Calcule e mostre a média das notas.
 
+import os
+os.system('cls')
+
+lista = []
+
 while (True):
 
-    lista = [ ]
-
     print('Digite "s" para sair.')
-    notas = str(input('Entre com as notas: '))
+    notas = str(input('Entre com as notas: ')).lower().strip()
 
     if notas != "s":
         print(f'Digite a proxíma.')
@@ -18,4 +21,15 @@ while (True):
         break
     
     lista.append(notas)
+print(f'A lista gerada é {lista}')
 
+quantidade = len(lista)
+
+print(f' A quantidade de notas inseridas no sistema foi: {quantidade}.')
+
+reversa = lista[::-1]
+
+for i in range(len(lista)):
+    print(f'notas invertidas {reversa[i]}')
+
+    
