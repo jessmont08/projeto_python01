b9975be5cd4c2e61080d21d1c6bcb1949d4820c3
jessmont08 <ster40028922@gmail.com>
2025-01-21 16:32:41 -0300

    atividade010

---
+++ atividade010/d.py
@@ -0,0 +1,14 @@
+import os
+
+
+os.system('cls')
+
+def converter_temp(fahrenheint):
+    celsius = (fahrenheint - 32) * 1.8
+
+    return celsius
+
+fahrenheint = float(input('Digite a temp em °F: '))
+
+celsius = converter_temp(fahrenheint)
+print(f'A temperatura {fahrenheint}°F em celsius é {celsius}°.')
