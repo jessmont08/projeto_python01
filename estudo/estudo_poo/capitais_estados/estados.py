7a052135849c0df089f4530fa314b9e54c6a6efa
jessmont08 <ster40028922@gmail.com>
2025-02-10 16:12:27 -0300

    aula12

--- estudo/estudo_poo/capitais_estados/estados.py
+++
@@ -1,9 +0,0 @@
-class Capitais():
-    def __init__(self, capitais):
-        self.capitais = ["rio branco", "maceio", "macapa", "manaus", "salvador", "fortaleza", 
-                         "brasilia", "vitoria", "goiania", "sao luis", "cuiaba", "campo grande", 
-                         "belo horizonte", "belem", "joao pessoa", "curitiba", "recife", "teresina", 
-                         "rio de janeiro", "natal", "porto alegre", "porto velho", "boa vista", 
-                         "florianopolis", "sao paulo", "aracaju", "palmas"]
-        
-        return capitais
